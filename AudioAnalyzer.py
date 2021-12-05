15f2543054cf6bf2adafc75a580c8abf1645f110
Farland <SteveZhao@outlook.com>
2021-12-05 20:56:25 +0800

    file err corrected & save image file

--- AudioAnalyzer.py
+++ AudioAnalyzer.py
@@ -8,9 +8,20 @@ from pydub import AudioSegment
 SECOND = 1000
 AUDIO_DIR = "Music" + os.path.sep
 CACHE_DIR = "Cache" + os.path.sep
-AUDIO_NAME = "IceCream.mp3"
+IMAGE_DIR = "Image" + os.path.sep
+AUDIO_NAME = "IceCream.wav"
+IMAGE_NAME = "IceCream.jpg"
 
-def split_music (begin, end, file_path):
+def make_file_dir(path, file_name):
+    path_exist = os.path.exists(path)
+    if not path_exist:
+        os.makedirs(path)
+    file = open(path + file_name, "w")
+    file.close
+
+def split_music(begin, end, audio_dir, cache_dir, audio_name):
+    file_path = audio_dir + audio_name
+    cache_path = cache_dir + audio_name
     print("[INFO]: file path is " + file_path)
     # Load the music
     song = AudioSegment.from_file(file_path)
@@ -18,7 +29,7 @@ def split_music (begin, end, file_path):
     # Get the splitted segment
     song = song[begin * SECOND: end * SECOND]
     # Save as cache
-    cache_path = CACHE_DIR + file_path
+    make_file_dir(cache_dir, audio_name)
     song.export(cache_path)
     return cache_path
 
@@ -26,11 +37,12 @@ if __name__ == '__main__':
     root = os.getcwd()
     AUDIO_DIR = root + os.path.sep + AUDIO_DIR
     CACHE_DIR = root + os.path.sep + CACHE_DIR
+    IMAGE_DIR = root + os.path.sep + IMAGE_DIR
     
     print("[INFO]: test dir " + AUDIO_DIR)
-    music, sr = librosa.load(split_music(0, 1, AUDIO_DIR + AUDIO_NAME))
-    # # music, sr = librosa.load(split_music(0, 15, AUDIO_NAME))
+    music, sr = librosa.load(split_music(0, 1, AUDIO_DIR, CACHE_DIR, AUDIO_NAME))
     # print a 14:5 Graph
     plt.figure(figsize=(14, 5))
     librosa.display.waveplot(music, sr=sr)
+    plt.savefig(IMAGE_DIR + IMAGE_NAME)
     plt.show()
